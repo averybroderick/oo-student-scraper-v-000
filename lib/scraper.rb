defebbc7b4b74ab22250a8d713b27d35dfb4d80b
Avery Broderick <avery.c.broderick@gmail.com>
2018-03-11 13:39:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,25 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+    students = page.css(".student-card")
+    stud_arr = []
+    students.each do |student|
+      stud_arr << {:name=> student.css(".student-name").text,
+                   :location=> student.css(".student-location").text,
+                   :profile_url=> student.css("a").attribute("href").value
+                  }
+    end
+    stud_arr
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    page = Nokogiri::HTML(open(profile_url))
+    student = {}
+    page.css(".social-icon-container").each do |social|
+      student[social] = social
+    end
+    student
   end
 
 end
-
